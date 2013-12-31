c0576462bbb0bc8119acf430dde8c231137dc323
Vladimir Stefan <psyren@neomailbox.ch>
2013-12-31 20:24:09 +0900

    added create database statement and further setup instructions for database superuser

--- src/db/superuser.sql
+++ src/db/superuser.sql
@@ -4,6 +4,19 @@
 create role ozevntsdev with login encrypted password 'test';
 create role ozevntsapp with login encrypted password 'test';
 
+create database ozevntsdb with 
+    owner            = ozevntsdev
+    encoding         = 'UTF8'
+    tablespace       = pg_default
+    lc_collate       = 'en_AU.UTF-8'
+    lc_ctype         = 'en_AU.UTF-8'
+    connection limit = -1;
+
+-- reconnect as superuser to ozevntsdb database
+
 CREATE SCHEMA ozevnts AUTHORIZATION ozevntsdev;
 GRANT ALL ON SCHEMA ozevnts TO ozevntsdev;
 GRANT USAGE ON SCHEMA ozevnts TO ozevntsapp;
+
+-- now connect as ozevntsdev and run the create_ddl.sql and create_functions.sql scripts
+
